7eb35f5fe22cd95dc3edb5a22c48b447f819c96c
Ravi Gupta <ravi720904@gmail.com>
2026-01-05 20:04:01 +0530

    Add files via upload

---
+++ Day1_p1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int a = 10;
+    int b = 3;
+    int c = -1;
+    int d = 20;
+    int sum = a+b;
+    int sum1 = c+d;
+    printf("The sum of a and b is: %d\n",sum);
+    printf("The sum of c and d is: %d\n",sum1);
+    return 0;
+}
+// #include <stdio.h>
+
+// int main(){
+//     int num,num1;
+//     printf("Enter two numbers = ");
+//     scanf("%d %d",&num,&num1);
+//     printf("The sum of %d and %d is: %d\n", num, num1, num+num1);
+//     return 0;
+// }
